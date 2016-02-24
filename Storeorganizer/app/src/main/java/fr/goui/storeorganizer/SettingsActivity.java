80c07441e1ea9a8a93a9d8a7d73bfe5d4db037b3
Galliano Guillaume <guillaume.gallianobondurand.ext@parrot.com>
2016-02-24 10:05:33 +0100

    Added Javadoc for SettingsActivity.

--- Storeorganizer/app/src/main/java/fr/goui/storeorganizer/SettingsActivity.java
+++ Storeorganizer/app/src/main/java/fr/goui/storeorganizer/SettingsActivity.java
@@ -2,6 +2,7 @@ package fr.goui.storeorganizer;
 
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -11,17 +12,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * {@code SettingsActivity} is the activity used to customize workers, tasks and other app's settings.
+ */
 public class SettingsActivity extends AppCompatActivity implements OnCategoryClickListener {
 
+    /**
+     * The constant for the workers' category.
+     */
     private static final int CATEGORY_WORKERS = 0;
+
+    /**
+     * The constant for the tasks' category.
+     */
     private static final int CATEGORY_TASKS = 1;
 
+    /**
+     * The {@code SharedPreferences}.
+     */
     private SharedPreferences mSharedPreferences;
+
+    /**
+     * The android resources to get project values.
+     */
+    private Resources mResources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // setting the layout
         setContentView(R.layout.activity_settings);
+
+        // getting the resources
+        mResources = getResources();
+
+        // setting up the action bar
         setupActionbar();
 
         // Check that the activity is using the layout version with
@@ -47,9 +73,13 @@ public class SettingsActivity extends AppCompatActivity implements OnCategoryCli
                     .add(R.id.fragment_settings_container, categoriesFragment).commit();
         }
 
+        // getting the shared prefs
         mSharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
     }
 
+    /**
+     * Method used to set up the action bar.
+     */
     private void setupActionbar() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -60,6 +90,8 @@ public class SettingsActivity extends AppCompatActivity implements OnCategoryCli
 
     @Override
     public void onCategoryClicked(int position_p) {
+
+        // creating the right fragment depending on the category clicked
         Fragment fragment = null;
         switch (position_p) {
             case CATEGORY_WORKERS:
@@ -70,6 +102,7 @@ public class SettingsActivity extends AppCompatActivity implements OnCategoryCli
                 break;
         }
 
+        // putting the selected fragment to the front
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_settings_container, fragment);
         transaction.addToBackStack(null);
@@ -85,40 +118,60 @@ public class SettingsActivity extends AppCompatActivity implements OnCategoryCli
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+
+        // if we press the up button going back
         if (id == android.R.id.home) {
             onBackPressed();
             return true;
         }
+
+        // if we press the restore default button restoring default
         if (id == R.id.action_restore_default) {
             restoreDefault();
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Method used to restore default for workers and tasks.
+     * An {@link AlertDialog} will be created asking for confirmation.
+     * Once confirmed all information about workers and tasks will be erased and a default worker and a default task will be created in both {@code SharedPreferences} and models.
+     */
     private void restoreDefault() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(getString(R.string.question_restore_default));
-        builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+        builder.setTitle(mResources.getString(R.string.question_restore_default));
+        builder.setPositiveButton(mResources.getString(R.string.ok), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                StoreWorkerModel.getInstance().clear(getString(R.string.worker));
-                StoreTaskModel.getInstance().clear(getString(R.string.task), 30);
 
+                // clearing models, it will create default worker and task
+                StoreWorkerModel.getInstance().clear(getString(R.string.worker));
+                StoreTaskModel.getInstance().clear(getString(R.string.task), mResources.getInteger(R.integer.task_default_duration));
+
+                // clearing shared prefs
                 SharedPreferences.Editor editor = mSharedPreferences.edit();
                 editor.clear();
 
-                editor.putInt(getString(R.string.worker_max_id), 0);
-                editor.putString(getString(R.string.worker) + 0, getString(R.string.worker));
-                editor.putInt(getString(R.string.task_max_id), 0);
-                editor.putString(getString(R.string.task) + 0, getString(R.string.task));
-                editor.putInt(getString(R.string.task) + 0 + getString(R.string.duration), 30);
+                // creating the default worker
+                editor.putInt(mResources.getString(R.string.worker_max_id), 0);
+                editor.putString(mResources.getString(R.string.worker) + 0, mResources.getString(R.string.worker));
+
+                // creating the default task
+                editor.putInt(mResources.getString(R.string.task_max_id), 0);
+                editor.putString(mResources.getString(R.string.task) + 0, mResources.getString(R.string.task));
+                editor.putInt(mResources.getString(R.string.task) + 0 + mResources.getString(R.string.duration),
+                        mResources.getInteger(R.integer.task_default_duration));
+
+                // putting them in the shared prefs
                 editor.apply();
             }
         });
-        builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+        builder.setNegativeButton(mResources.getString(R.string.cancel), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                // if cancel button was pressed discarding the dialog
                 dialog.cancel();
             }
         });
